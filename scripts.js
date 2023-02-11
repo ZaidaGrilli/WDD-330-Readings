1c6ee7c4597127c7dce0a5ecf8b7302180fd181f
ZaidaGrilli <grillizaida@gmail.com>
2023-02-11 14:54:53 -0500

    .

--- scripts.js
+++ scripts.js
@@ -18,19 +18,18 @@ let imageGalleryTemplate = function imageGalleryTemplate (imgs) {
         return imgTemp;
 }
 
-//insert week number into header
+
 document.getElementById('wkNumHead').textContent += "Week 06 Notes";
 
-// scripts to enter notes into the DOM
 function notesToHtml(obj, id, elemType, note, cls) {
   let insHere = document.getElementById(`${id}`);
   let elem = document.createElement(`${elemType}`);
-  //set default to none and make it so that class is optional 
+
   if (typeof(cls) != 'undefined') {
       this.cls = cls;
       elem.className = cls;
   }
-  //set note to optional put in before if it it there and leave it out if it's not
+
   if (typeof(note) != 'undefined') {
     this.note = note;
     elem.innerHTML = note + obj;
@@ -42,8 +41,7 @@ function notesToHtml(obj, id, elemType, note, cls) {
   insHere.append(elem);
 }
 
-//----------------------------------------------------------Notes
-//get the whole form
+
 let form = document.forms.my;
 
 notesToHtml(form,'mainInsert', 'p', '(see console for HTML notes) Get the whole form: ');
@@ -53,55 +51,53 @@ console.log(form);
 let elem = form.elements.one;
 console.log(elem);
 
-//elements with the same name
+
 let sameElemName = form.elements.age;
-console.log(sameElemName); //this prints a node list
+console.log(sameElemName); 
 console.log(sameElemName[0]);
-console.log(sameElemName[1]);// these return the individual elements
+console.log(sameElemName[1]);
 
 let fieldSet = form.elements.login;
 console.log(fieldSet); 
 
-notesToHtml(my.login == form.login, 'mainInsert', 'p', 'my.login == form.login: '); //true
+notesToHtml(my.login == form.login, 'mainInsert', 'p', 'my.login == form.login: '); 
 
-//accessing values
-notesToHtml(sameElemName[0].value, 'mainInsert', 'p','Values: '); // .value acces the value instead of the HTML
+
+notesToHtml(sameElemName[0].value, 'mainInsert', 'p','Values: ');
 notesToHtml(sameElemName[1].value, 'mainInsert', 'p','Values: ');
-notesToHtml(sameElemName[0].checked, 'mainInsert', 'p','Values: ');//.checked will return true or false depending on weather the radio button or box is filled.
+notesToHtml(sameElemName[0].checked, 'mainInsert', 'p','Values: ');
 
-//select and option
-select.options[2].selected = true; //options are zero indexed like arrays so the third is 2
+
+select.options[2].selected = true; 
 select.selectedIndex = 1;
 select.value = 'banana' 
 
-//get all that are selected when multiple are allowed
+
 let selected = Array.from(selectMult.options)
                     .filter(option => option.selected)
                     .map(option => option.value);
 
 notesToHtml(selected, "mainInsert", 'p', 'Selected from music options: ', 'mkItGreen');
 
-//add new (unselected)option to the list ** to add a selected property add ' ,true ,true' at the end
-let option = new Option('text', 'value',true,true);//creates <option value='value'> TEXT </option>
+
+let option = new Option('text', 'value',true,true);
 console.log(option);
 
 
-//------------------------FORMDATA Objects
-//event listener
+
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('formObject').addEventListener('submit', handleForm);
 });
 
-//function to handle info inside the form
+
 
 function handleForm(ev) {
-  ev.preventDefault(); //This wioll stop the page from relaoding *** VERY IMPORTANT***
+  ev.preventDefault(); 
   console.log(ev.target);
 
   let myForm = ev.target;
   let fd = new FormData(formObject);
 
-  //add more things that were not in the form
   fd.append('api-key', 'myApiKey');
 
   //look at all the contents
@@ -109,7 +105,7 @@ function handleForm(ev) {
     console.log(key, fd.get(key));
   }
 
-  //send the request with the formdata
+
   
 } 
 
